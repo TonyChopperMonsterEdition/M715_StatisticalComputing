d141556b5e69c6054987a88d1c36d8bfde334bad
david <devilthea@gmail.com>
2023-05-11 21:14:51 +0200

    Neues Skript erstellt und mit WD gearbeitet

--- M715_Hausarbeit/00_Skripte/test.r
+++ M715_Hausarbeit/00_Skripte/test.r
@@ -4,3 +4,14 @@ library(tidyverse)
 library(ggplot2)
 library(knitr)
 library(rmarkdown)
+
+
+setwd("~/RStudio/M715_StatisticalComputing/")
+getwd()
+
+ggplot(diamonds, aes(carat, price)) + 
+  geom_hex()
+getwd()
+ggsave("diamonds.png", path = paste(getwd(),'/Lerneinheiten'))
+
+write.csv(diamonds, paste(getwd(),"/Lerneinheiten/diamonds.csv"))
