6bbd20456c579a6d7c454c0341d52df75680a7d1
david <devilthea@gmail.com>
2023-06-09 14:06:41 +0200

    Laden und anpassen der Kriminalfälle 2016 werte anpassen

--- M715_Hausarbeit/00_Skripte/read_datafiles.R
+++ M715_Hausarbeit/00_Skripte/read_datafiles.R
@@ -1,6 +1,6 @@
 caseNumbers <- c()
 caseNumbers_FileLocation <- c()
-#orkingDir="./M715_Hausarbeit/01_Datenquellen/" # Tests for script only run
+#workingDir="./M715_Hausarbeit/01_Datenquellen/" # Tests for script only run
 workingDir="./01_Datenquellen/" # Line for run over rmarkdown
 
 
@@ -18,5 +18,26 @@ seq_along(caseNumbers)
 ###########################Extract#####################################
 
 for(i in seq_along(caseNumbers)){
-  assign(caseNumbers[i], read_csv2(caseNumbers_FileLocation[i],skip=1, na= c("","NA"),col_names= TRUE, col_select=(2:4), show_col_types = FALSE, locale = locale(encoding = 'latin1', decimal_mark = ",",  grouping_mark = ".")))
+  assign(caseNumbers[i], read_csv2(caseNumbers_FileLocation[i],skip=1, na= c("","NA"),col_names= TRUE, col_select=(2:5), show_col_types = FALSE, locale = locale(encoding = 'latin1')))
 }
+
+### Renames the necessary columns from german to english names
+for(x in seq_along(caseNumbers)){
+  var=get(caseNumbers[x])
+  colnames(var)[1:3]=c("Crimetype",  "FederalState", "Casenumbers")
+  assign(caseNumbers[x],var)
+  
+  if (identical(var,caseNumbers_2016))
+  {
+    colnames(caseNumbers_2016)[3:4] <- colnames(caseNumbers_2016)[2:3]
+    caseNumbers_2016 <- caseNumbers_2016[,-2]
+  }
+  else
+  {
+    var <- var[,-4]
+    assign(caseNumbers[x],var)
+  }
+}
+
+#var=get(caseNumbers[1])
+#var <- var[,-4]
