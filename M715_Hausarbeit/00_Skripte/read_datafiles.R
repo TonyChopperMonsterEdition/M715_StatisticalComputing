ea77352bfffd4deb5e08d555684051428da9e6e8
david <devilthea@gmail.com>
2023-06-17 01:01:47 +0200

    - Formatierung der Zahlen Problem gelöst durch entfernen von Komma - Tabellen für Abschnitt 2.1. erstellt und text grob geschrieben --> Formatierung der Tabellen ausstehend.

--- M715_Hausarbeit/00_Skripte/read_datafiles.R
+++ M715_Hausarbeit/00_Skripte/read_datafiles.R
@@ -14,30 +14,40 @@ length(caseNumbers)
 seq_along(caseNumbers)
 
 
-
 ###########################Extract#####################################
 
 for(i in seq_along(caseNumbers)){
-  assign(caseNumbers[i], read_csv2(caseNumbers_FileLocation[i],skip=1, na= c("","NA"),col_names= TRUE, col_select=(2:5), show_col_types = FALSE, locale = locale(encoding = 'latin1')))
-}
+  # reads the tables and also the case numbers as character. Especially the Casenumbers has to be read as characters to prevent that r appends  zero at the end of some numbers
+  # in a preversion of this script, importing the tables causes that at the end of numbers of 3 digit size additional zeros was added, while comma was interpreted as decimal instead of bigmark.
+  assign(caseNumbers[i], read_csv2(caseNumbers_FileLocation[i],skip=1, col_types= "ccccc", na= c("","NA"),col_names= TRUE, col_select=(1:5), show_col_types = FALSE, trim_ws=TRUE, locale = locale(encoding = 'latin1', decimal_mark=",")))
+  #assign(caseNumbers[i], read_delim(caseNumbers_FileLocation[i],skip=1, , delim= ";", na= c("","NA"),col_names= TRUE, col_select=(1:5), show_col_types = FALSE, trim_ws=TRUE, locale = locale(encoding = 'latin1', decimal_mark=",")))
+  #assign(caseNumbers[i], as_tibble(read.csv2(caseNumbers_FileLocation[i],skip=1, sep =";", na.strings= c("","NA"),header= TRUE, encoding = 'latin1', dec="."))[1:5])
+  }
 
 ### Renames the necessary columns from german to english names
 for(x in seq_along(caseNumbers)){
   var=get(caseNumbers[x])
-  colnames(var)[1:3]=c("Crimetype",  "FederalState", "Casenumbers")
+  colnames(var)[1:4]=c("CrimeKey","Crimetype",  "FederalState", "Casenumbers")
   assign(caseNumbers[x],var)
+  
   
   if (identical(var,caseNumbers_2016))
   {
-    colnames(caseNumbers_2016)[3:4] <- colnames(caseNumbers_2016)[2:3]
-    caseNumbers_2016 <- caseNumbers_2016[,-2]
+    colnames(caseNumbers_2016)[4:5] <- colnames(caseNumbers_2016)[3:4]
+    caseNumbers_2016 <- caseNumbers_2016[,-3]
   }
   else
   {
-    var <- var[,-4]
+    var <- var[,-5]
     assign(caseNumbers[x],var)
   }
 }
-
+## needed to format the Casenumbers from string to numeric data type
+for(x in seq_along(caseNumbers)){
+ var=get(caseNumbers[x])
+ var$Casenumbers = gsub(",","",var$Casenumbers)
+ var$Casenumbers = as.numeric(var$Casenumbers)
+ assign(caseNumbers[x],var)
+}
 #var=get(caseNumbers[1])
 #var <- var[,-4]
